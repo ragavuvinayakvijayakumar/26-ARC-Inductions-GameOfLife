440b47266e91e5f181493491a2c2b7b04f5cebc3
Vinayak_vijayakumar <11hype1r1son1ics@gmail.com>
2026-09-02 19:23:03 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,18 +1,13 @@
 import math 
 #---------------------------- TASK 1 ----------------------------
 def count_neighbors(grid, row, col):
-    len_column = len(grid[0])
-    len_row = len(grid)
-    min_row = max(row-1,0)
-    min_column = max(column-1,0) 
-    max_row = min(row+1,len_row)
-    max_column = min(column+1,len_column)
+    len_column,len_row  = len(grid[0]),len(grid)
+    min_row,min_column  = max(row-1,0), max(col-1,0)
+    max_row,max_column= min(row+1,len_row-1),min(col+1,len_column-1)
     alive_count = 0
     for i in range(min_row,max_row+1):
         for j in range(min_column,max_column+1):
-            alive_count += grid[j][i]
-            j++
-        i++
+            alive_count += grid[i][j]
     alive_count -= grid[row][col]
     return alive_count 
     
@@ -53,8 +48,8 @@ def compute_next_generation(grid):
     next_grid = [[0 for _ in range(cols)] for _ in range(rows)]
     for i in range(0,rows):
         for j in range(0,cols):
-            alive_count = count_neighours(grid,i,j)
-            if gird[i][j] = 1:
+            alive_count = count_neighbors(grid,i,j)
+            if grid[i][j] == 1:
                 if alive_count <2:
                     next_grid[i][j] = 0
                 if 1< alive_count < 4 :
@@ -62,6 +57,7 @@ def compute_next_generation(grid):
                 if 3< alive_count:
                     next_grid[i][j] = 0
             else:
-                if alive_count =3 :
+                if alive_count == 3 :
                     next_grid[i][j] = 1
+                    
     return next_grid
