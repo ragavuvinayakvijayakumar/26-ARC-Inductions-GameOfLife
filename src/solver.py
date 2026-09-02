bdf7241e2f433bf7e8781287f699f95d31c84d8e
Vinayak_vijayakumar <11hype1r1son1ics@gmail.com>
2026-09-02 20:19:11 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,5 +1,3 @@
-import math 
-#---------------------------- TASK 1 ----------------------------
 def count_neighbors(grid, row, col):
     len_column,len_row  = len(grid[0]),len(grid)
     min_row,min_column  = max(row-1,0), max(col-1,0)
@@ -11,40 +9,9 @@ def count_neighbors(grid, row, col):
     alive_count -= grid[row][col]
     return alive_count 
     
-    """
-    Counts the number of alive neighbors for a specific cell in the grid.
-    A cell can have up to 8 neighbors (horizontal, vertical, and diagonal).
-    
-    Args:
-        grid (list of lists): The current 2D state of the game.
-        row (int): The row index of the cell.
-        col (int): The column index of the cell.
-        
-    Returns:
-        int: The total number of alive neighbors (0 to 8).
-    """
-
-#---------------------------- TASK 2 ----------------------------
 def compute_next_generation(grid):
-    """
-    Generates the next state of the grid based on Conway's rules.
-    
-    Args:
-        grid (list of lists): The current 2D state of the game.
-        
-    Returns:
-        list of lists: A BRAND NEW 2D grid representing the next generation.
-        
-    Note:
-        - Do NOT modify the original `grid` directly while iterating through it. 
-          You must create a new grid to store the updated states, otherwise 
-          your changes will mess up the neighbor counts for subsequent cells!
-    """
-    
     rows = len(grid)
     cols = len(grid[0]) if rows > 0 else 0
-    
-    # Create a new blank grid of the same size, filled with 0s (dead cells)
     next_grid = [[0 for _ in range(cols)] for _ in range(rows)]
     for i in range(0,rows):
         for j in range(0,cols):
