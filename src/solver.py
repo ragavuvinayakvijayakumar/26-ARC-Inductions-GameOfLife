181c412227bedf9e3a8dcd366b545c61dbf28221
Vinayak_vijayakumar <11hype1r1son1ics@gmail.com>
2026-09-02 19:06:11 +0530

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,5 +1,21 @@
+import math 
 #---------------------------- TASK 1 ----------------------------
 def count_neighbors(grid, row, col):
+    len_column = len(grid[0])
+    len_row = len(grid)
+    min_row = max(row-1,0)
+    min_column = max(column-1,0) 
+    max_row = min(row+1,len_row)
+    max_column = min(column+1,len_column)
+    alive_count = 0
+    for i in range(min_row,max_row+1):
+        for j in range(min_column,max_column+1):
+            alive_count += grid[j][i]
+            j++
+        i++
+    alive_count -= grid[row][col]
+    return alive_count 
+    
     """
     Counts the number of alive neighbors for a specific cell in the grid.
     A cell can have up to 8 neighbors (horizontal, vertical, and diagonal).
@@ -12,12 +28,6 @@ def count_neighbors(grid, row, col):
     Returns:
         int: The total number of alive neighbors (0 to 8).
     """
-    
-    alive_count = 0
-    
-    # TODO: Implement your neighbor-counting logic here!
-
-    return alive_count
 
 #---------------------------- TASK 2 ----------------------------
 def compute_next_generation(grid):
@@ -41,9 +51,17 @@ def compute_next_generation(grid):
     
     # Create a new blank grid of the same size, filled with 0s (dead cells)
     next_grid = [[0 for _ in range(cols)] for _ in range(rows)]
-    
-    # TODO: Iterate through every cell in the `grid`.
-    # TODO: Use your `count_neighbors` function to find out how many neighbors it has.
-    # TODO: Apply the 4 Rules of Life to determine if it should be 1 (alive) or 0 (dead) in `next_grid`.
-
+    for i in range(0,rows):
+        for j in range(0,cols):
+            alive_count = count_neighours(grid,i,j)
+            if gird[i][j] = 1:
+                if alive_count <2:
+                    next_grid[i][j] = 0
+                if 1< alive_count < 4 :
+                    next_grid[i][j] = 1
+                if 3< alive_count:
+                    next_grid[i][j] = 0
+            else:
+                if alive_count =3 :
+                    next_grid[i][j] = 1
     return next_grid
